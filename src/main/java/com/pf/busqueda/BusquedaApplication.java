1ab426a37236db17a6c449361d317e7f9e15dca6
Juan Nicolau Sepúlveda <jnicolau@iti.es>
2019-05-26 20:35:42 +0200

    Import inicial

---
+++ src/main/java/com/pf/busqueda/BusquedaApplication.java
@@ -0,0 +1,13 @@
+package com.pf.busqueda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusquedaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusquedaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pf/busqueda/BusquedaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pf.busqueda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BusquedaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
